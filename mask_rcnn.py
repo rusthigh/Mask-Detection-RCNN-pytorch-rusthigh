c8256ac5e57f59004e5e6a8426a311c24e4f4924
rusthigh <rusthigh@proton.me>
2022-06-22 20:58:57 +0000

    Update SoftwareToInstall.md

--- mask_rcnn.py
+++ mask_rcnn.py
@@ -13,4 +13,8 @@ import numpy as np
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 font = cv2.FONT_HERSHEY_SIMPLEX 
-fontScale =
+fontScale = 2
+thickness = 3
+
+
+def get_instance_segmentation_model(
