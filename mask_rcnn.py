fe5da9d75e4fcac1ff0beab61bfbe3a485d291c5
rusthigh <rusthigh@proton.me>
2022-06-22 18:26:02 +0000

    Created readme

--- mask_rcnn.py
+++ mask_rcnn.py
@@ -7,4 +7,8 @@ import torch
 import torchvision
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
-from torchvision.transforms import functional as 
+from torchvision.transforms import functional as F
+import cv2
+import numpy as np
+
+device = torch.device('cuda') if
