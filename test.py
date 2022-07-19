14dc52598505600daf611ef6dfabc48678185ce1
rusthigh <rusthigh@proton.me>
2022-07-19 21:27:17 +0000

    fixed pom.xml

--- test.py
+++ test.py
@@ -2,4 +2,7 @@ import os
 import cv2
 import argparse
 import matplotlib.pyplot as plt
-from mask_rcnn import segmentation_model, plot_mask
+from mask_rcnn import segmentation_model, plot_masks
+
+if __name__ == '__main__':
+    parser = argpar
