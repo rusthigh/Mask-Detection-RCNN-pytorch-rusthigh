d0b67a9f9fe846bf618cd1740645f3c7993646e1
rusthigh <rusthigh@proton.me>
2022-07-20 07:11:50 +0000

    Update angles.js

--- test.py
+++ test.py
@@ -6,4 +6,5 @@ from mask_rcnn import segmentation_model, plot_masks
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--img', type=str, default='test_image.jpg', help='path to your te
+    parser.add_argument('--img', type=str, default='test_image.jpg', help='path to your test image')
+    parser.add_argument('--labels', type=str, 
