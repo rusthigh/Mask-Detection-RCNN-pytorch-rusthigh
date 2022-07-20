7ae69edfcf87f2fa7ca7ccfa53e2d6503850028f
rusthigh <rusthigh@proton.me>
2022-07-20 17:22:02 +0000

    Updated prototyping.md

--- test.py
+++ test.py
@@ -8,4 +8,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--img', type=str, default='test_image.jpg', help='path to your test image')
     parser.add_argument('--labels', type=str, default='./my_dataset/labels.txt', help='path to labels list text file (labels.txt)')
-    parser.add_argument('--model', type=str, default='./maskrcnn_saved_models/mask_rcnn_model.pt', help='path to saved mod
+    parser.add_argument('--model', type=str, default='./maskrcnn_saved_models/mask_rcnn_model.pt', help='path to saved model')
+
+    args = parser.parse_args()
+    
+    IM
