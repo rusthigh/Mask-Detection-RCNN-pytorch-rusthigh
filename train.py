c124d9049748857d6b584d015cd6360534044337
rusthigh <rusthigh@proton.me>
2022-08-02 06:53:22 +0000

    Add help page

--- train.py
+++ train.py
@@ -5,4 +5,7 @@ import argparse
 import utils.utils
 from utils.engine import train_one_epoch, evaluate
 from utils.dataset import maskrcnn_Dataset, get_transform
-from utils.model i
+from utils.model import get_instance_segmentation_model
+
+
+if __name__ == 
