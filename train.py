40653fa1a0717285544a470856262fe522aeeedc
rusthigh <rusthigh@proton.me>
2022-08-02 22:59:52 +0000

    Added:

--- train.py
+++ train.py
@@ -11,4 +11,5 @@ from utils.model import get_instance_segmentation_model
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', type=str, default='my_dataset', help='dataset path')
-    parser.add_argument('--num_classes', type=int, default=11, help='number of classes (background as a class)
+    parser.add_argument('--num_classes', type=int, default=11, help='number of classes (background as a class)')
+    parser.add_argument('--num_epochs', type=
