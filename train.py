8dcc08f117e641622c3e46084ba47e2c6696aea3
rusthigh <rusthigh@proton.me>
2022-08-05 10:37:34 +0000

    updated menu

--- train.py
+++ train.py
@@ -12,4 +12,5 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', type=str, default='my_dataset', help='dataset path')
     parser.add_argument('--num_classes', type=int, default=11, help='number of classes (background as a class)')
-    parser.add_argument('--num_epochs', type=
+    parser.add_argument('--num_epochs', type=int, default=150, help='number of epochs')
+    parser.a
