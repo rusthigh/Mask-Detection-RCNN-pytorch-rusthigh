73ede7541e01c697d885c9e19f91165ec22db6fc
rusthigh <rusthigh@proton.me>
2022-09-01 20:14:59 +0000

    Release 0.12.0

--- utils/transforms.py
+++ utils/transforms.py
@@ -5,4 +5,5 @@ from torchvision.transforms import functional as F
 
 
 def _flip_coco_person_keypoints(kps, width):
-    flip_inds = [0, 2, 1, 4, 3, 6, 5, 8, 7, 10, 9, 12, 11
+    flip_inds = [0, 2, 1, 4, 3, 6, 5, 8, 7, 10, 9, 12, 11, 14, 13, 16, 15]
+    flipped_data = kps[:, flip_inds]
