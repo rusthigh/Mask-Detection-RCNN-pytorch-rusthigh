0884ee46834955d4830b32a88e5010c6a9ff4c08
rusthigh <rusthigh@proton.me>
2022-09-01 07:39:46 +0000

    2.4.4

--- utils/transforms.py
+++ utils/transforms.py
@@ -1,4 +1,7 @@
 import random
 import torch
 
-from torchvision.transforms import functional as 
+from torchvision.transforms import functional as F
+
+
+def _flip_coco_person_keypoints(kps, width):
